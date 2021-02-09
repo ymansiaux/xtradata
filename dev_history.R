4ff1a4618664b7398d0cf2c1377dd417431b097f
Yohann Mansiaux <y.mansiaux@bordeaux-metropole.fr>
2021-02-09 10:10:29 +0100

    misc

--- dev_history.R
+++ dev_history.R
@@ -2,3 +2,8 @@ usethis::use_build_ignore(files = "dev_history.R")
 usethis::use_gpl3_license()
 
 usethis::use_vignette("xtradata")
+
+usethis::use_pipe()
+
+usethis::use_package("httr")
+usethis::use_package("assertthat")
